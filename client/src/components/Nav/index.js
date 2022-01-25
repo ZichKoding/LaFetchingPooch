94491247f0aa6241682973ee54bbd0218ca01916
Chris Zichko <chriszichkocoding@gmail.com>
2022-01-24 20:35:26 -0500

    Got React up and running

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <header>
+            <h1>LaFetching Pooch</h1>
+            <ul>
+                <li>
+                    Home
+                </li>
+                <li>
+                    About Us
+                </li>
+                    Mobile Services
+                <li>
+                    LaFetching Pooch Social Media
+                </li>
+                <li>
+                    Contact Us
+                </li>
+            </ul>
+        </header>
+    )
+}
+
+export default Nav;
