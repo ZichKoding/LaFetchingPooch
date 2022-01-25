fcd67561161b5801ab859c6795af3de078eb3c3f
Chris Zichko <chriszichkocoding@gmail.com>
2022-01-25 15:07:22 -0500

    Working on nav Style

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,26 +1,30 @@
 import React from "react";
+import logo from '../../images/LadyRedDressWalkingDog.jpg';
 
 function Nav() {
+    const navcontent = [
+        "Home",
+        "About Us",
+        "Mobile Services",
+        "La Fetching Pooch's Social Media",
+        "Contact Us"
+    ];
+
     return (
-        <header>
-            <h1>LaFetching Pooch</h1>
-            <ul>
-                <li>
-                    Home
-                </li>
-                <li>
-                    About Us
-                </li>
-                    Mobile Services
-                <li>
-                    LaFetching Pooch Social Media
-                </li>
-                <li>
-                    Contact Us
-                </li>
+        <header className="container bg-blue font-white">
+            <a href="/" className="biz-container font-white no-a-style">
+                <img src={logo} alt="logo" className="logo" />
+                <h1 className="business-title font-sm">
+                    La Fetching Pooch
+                </h1>
+            </a>
+            <ul className="container nav-ul">
+                {navcontent.map((content) => (
+                    <li className=".font-sm no-li-style nav-links">{content}</li>
+                ))}
             </ul>
         </header>
-    )
-}
+    );
+};
 
 export default Nav;
