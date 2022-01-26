150a0365c3facd6fc46bf083184f3030adb678ba
Chris Zichko <chriszichkocoding@gmail.com>
2022-01-26 15:21:29 -0500

    Header/Navbar finished?

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,28 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from '../../images/LadyRedDressWalkingDog.jpg';
+import { Container, Row, Col, Dropdown, DropdownButton } from "react-bootstrap";
 
 function Nav() {
+    const [state, setState] = useState(0);
     const navcontent = [
         "Home",
         "About Us",
         "Mobile Services",
-        "La Fetching Pooch's Social Media",
+        "Social Media",
         "Contact Us"
     ];
 
+    let screenWidth;
+
+    window.addEventListener('resize', async () => {
+        try {
+            screenWidth = await window.screen.width;
+            setState(screenWidth);
+        }
+        catch (e) {
+            console.error(e);
+        }
+    })
+
     return (
-        <header className="container bg-blue font-white">
-            <a href="/" className="biz-container font-white no-a-style">
-                <img src={logo} alt="logo" className="logo" />
-                <h1 className="business-title font-sm">
-                    La Fetching Pooch
-                </h1>
-            </a>
-            <ul className="container nav-ul">
-                {navcontent.map((content) => (
-                    <li className="font-sm no-li-style nav-links"><span className="li-span">{content}</span></li>
-                ))}
-            </ul>
+        <header>
+            <Container fluid className="bg-blue">
+                <Row>
+                    {/* <Col xs={10} sm={4} className="center-title"> */}
+                    <Col xs={12} sm={4} md={4} lg={4} className="center-title">
+                        <a href="/" className="no-a-style font-white">
+                            <h1>
+                                La Fetching Pooch, LLC
+                            </h1>
+                            <p>Mobile Dog Grooming Services</p>
+                        </a>
+                    </Col>
+                    <Col xs={9} sm={4} md={6} lg={6}>
+                        {state <= 1250 ? (
+                            <Container>
+                                <Row>
+                                    <Col xs={6} sm={6}>
+                                        <DropdownButton id="dropdown-basic-button" title="&equiv;" className="text-right">
+                                            {navcontent.map((content) => (
+                                                <Dropdown.Item href={`#${content}`} key={content} className="nav-drop-center">
+                                                    <span className="li-span">
+                                                        {content}
+                                                    </span>
+                                                </Dropdown.Item>
+                                            ))}
+                                        </DropdownButton> 
+                                    </Col>
+                                    <Col xs={6} sm={6} className="center-logo">
+                                        <a href="/" className="no-a-style font-white">
+                                            <img src={logo} alt="logo" className="logo" />
+                                        </a>
+                                    </Col>
+                                </Row>
+                            </Container> 
+                        ) : (
+                            <Container className="font-white font-sm font-b-style">
+                                <Row>
+                                    {navcontent.map((content) => (
+                                        <Col key={content} className="nav-content">
+                                            <span className="li-span">
+                                                {content}
+                                            </span>
+                                        </Col>
+                                    ))}
+                                    <Col className="center-logo">
+                                        <a href="/" className="no-a-style font-white">
+                                            <img src={logo} alt="logo" className="logo" />
+                                        </a>
+                                    </Col>
+                                </Row>
+                            </Container>
+                        )}
+                        
+                    </Col>
+
+                </Row>
+            </Container>
         </header>
     );
 };
