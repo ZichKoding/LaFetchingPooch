a6c6e4eae264f745054d49023b9966b61ff94800
Chris Zichko <chriszichkocoding@gmail.com>
2022-02-13 14:20:08 -0500

    Positioning and carousel functioning

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Container } from 'react-bootstrap';
 
 function Homepage () {
     return (
         <>
-            <Carousel>
-                <Carousel.Item>
-                    <img 
-                        className='caro-slide-pics'
-                        src={require('../assets/images/IMG_20210411_072537_276.jpg')} 
-                        alt="First Slide" 
-                    />
-                    <Carousel.Caption>
-                        <h3>Mobile Pet Grooming Services</h3>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+            <Container fluid>
+                <Carousel className='center-items'>
+                    <Carousel.Item>
+                        <img 
+                            className='caro-slide-pics'
+                            src={require('../assets/images/IMG_20210411_072537_276.jpg')} 
+                            alt="First Slide" 
+                        />
+                        <Carousel.Caption>
+                            <h3>Mobile Pet Grooming Services</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img 
+                            className='caro-slide-pics'
+                            src={require('../assets/images/20210513_120344.jpg')} 
+                            alt="First Slide" 
+                        />
+                        <Carousel.Caption>
+                            <h3>Mobile Pet Grooming Services</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </Container>
         </>
     );
 };
