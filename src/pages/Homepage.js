26a95c0fd4952a34d8f4e24fb1a572a1015c8bad
Chris Zichko <chriszichkocoding@gmail.com>
2022-02-14 10:37:42 -0500

    Adding photos to carousel

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,30 +2,47 @@ import React from 'react';
 import { Carousel, Container } from 'react-bootstrap';
 
 function Homepage () {
+    const photos = [
+        {
+            pic: 'carousel1.jpg',
+            description: 'Lorem Ipsum moufh laksdnf lkajsdf;ljc hasdfh asdfn, huiysdf fksjh qcyut ancyt  adhhcyh asdflkj.'
+        },
+        {
+            pic: 'carousel2.jpg',
+            description: 'Lorem Ipsum moufh laksdnf lkajsdf;ljc hasdfh asdfn, huiysdf fksjh qcyut ancyt  adhhcyh asdflkj.'
+        },
+        {
+            pic: 'carousel3.jpg',
+            description: 'Lorem Ipsum moufh laksdnf lkajsdf;ljc hasdfh asdfn, huiysdf fksjh qcyut ancyt  adhhcyh asdflkj.'
+        },
+        {
+            pic: 'carousel4.jpg',
+            description: 'Lorem Ipsum moufh laksdnf lkajsdf;ljc hasdfh asdfn, huiysdf fksjh qcyut ancyt  adhhcyh asdflkj.'
+        },
+        {
+            pic: 'carousel5.jpg',
+            description: 'Lorem Ipsum moufh laksdnf lkajsdf;ljc hasdfh asdfn, huiysdf fksjh qcyut ancyt  adhhcyh asdflkj.'
+        },
+        
+    ];
+
     return (
         <>
             <Container fluid>
                 <Carousel className='center-items'>
-                    <Carousel.Item>
-                        <img 
-                            className='caro-slide-pics'
-                            src={require('../assets/images/IMG_20210411_072537_276.jpg')} 
-                            alt="First Slide" 
-                        />
-                        <Carousel.Caption>
-                            <h3>Mobile Pet Grooming Services</h3>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img 
-                            className='caro-slide-pics'
-                            src={require('../assets/images/20210513_120344.jpg')} 
-                            alt="First Slide" 
-                        />
-                        <Carousel.Caption>
-                            <h3>Mobile Pet Grooming Services</h3>
-                        </Carousel.Caption>
-                    </Carousel.Item>
+                    {photos.map(photo => (
+                        <Carousel.Item>
+                            <img 
+                                className='caro-slide-pics'
+                                src={require(`../assets/images/${photo.pic}`)} 
+                                alt="First Slide" 
+                            />
+                            <Carousel.Caption>
+                                <h3>{photo.description}</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    ))}
+                    
                 </Carousel>
             </Container>
         </>
